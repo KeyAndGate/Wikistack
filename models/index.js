6179c0ad80359d72529313baa1ffec2e5ab92220
Whitman Johnson <whit.ross.j@gmail.com>
2018-01-22 11:03:18 -0500

    beginning step 4

--- models/index.js
+++ models/index.js
@@ -1,16 +1,56 @@
-var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var Sequelize = require('sequelize')
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    // logging: false
+})
 
-// page model
-{
-    title: Sequelize.STRING,
-    urlTitle: Sequelize.STRING,
-    content: Sequelize.TEXT,
-    status: Sequelize.BOOELEAN
-}
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isUrl: true
+        }
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        defaultValue: 'this article is a stub, you can help by expanding it.'
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+},{
+    getterMethods: {
+        route(){
+            return '/wiki/' + this.urlTitle
+        }
+    }
+})
 
-// user model
-{
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+})
+
+module.exports = {
+    Page,
+    User,
+    db
 }
